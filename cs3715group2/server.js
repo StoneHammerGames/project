1bb64d9431cdbaf78975d249b63afc6b9be9cd25
idefilippisv <idefilippisv@devastator.cs.mun.ca>
2017-11-21 09:49:11 -0330

    server.js added

--- cs3715group2/server.js
+++ cs3715group2/server.js
@@ -4,6 +4,6 @@
 var http = require("http");
 http.createServer(function(request, response) {
 	response.writeHead(200,{"Content-Type": "text/plain"});
-	response.write("Hello World 3");
+	response.write("Hello World 4");
 	response.end();
 }).listen(8888);
